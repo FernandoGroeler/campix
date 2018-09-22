6eb7cd8582da6d7c3c175a3f237a67614e0221c1
Pedro Dorini <pedro.dorini@hotmail.com>
2018-09-22 14:41:08 -0300

    remover artefatos não mais utilizados

--- app/src/main/java/io/trabalho/eletiva/campix/ui/activity/BaseDrawerActivity.java
+++ app/src/main/java/io/trabalho/eletiva/campix/ui/activity/BaseDrawerActivity.java
@@ -1,10 +1,7 @@
 package io.trabalho.eletiva.campix.ui.activity;
 
-import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
-import android.view.Gravity;
 import android.view.LayoutInflater;
-import android.view.View;
 import android.view.ViewGroup;
 import butterknife.BindView;
 import io.trabalho.eletiva.campix.R;
@@ -13,8 +10,6 @@ public class BaseDrawerActivity extends BaseActivity {
 
     @BindView(R.id.drawerLayout)
     DrawerLayout drawerLayout;
-    @BindView(R.id.vNavigation)
-    NavigationView vNavigation;
 
     @Override
     public void setContentView(int layoutResID) {
@@ -27,13 +22,5 @@ public class BaseDrawerActivity extends BaseActivity {
     @Override
     protected void setupToolbar() {
         super.setupToolbar();
-        if (getToolbar() != null) {
-            getToolbar().setNavigationOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    drawerLayout.openDrawer(Gravity.LEFT);
-                }
-            });
-        }
     }
 }
